72a48c382fa245dbe2ff1f68749d0abb90a07ab5
mfathur <mfathur17@gmail.com>
2021-01-24 08:54:19 +0700

    add item recycler suggested layout

--- app/src/main/java/com/purplepotato/kajianku/core/domain/Kajian.kt
+++ app/src/main/java/com/purplepotato/kajianku/core/domain/Kajian.kt
@@ -1,5 +1,9 @@
 package com.purplepotato.kajianku.core.domain
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Kajian(
-    val id : Int
-)
+    val id: Int
+) : Parcelable
---
+++ app/src/main/java/com/purplepotato/kajianku/saved_kajian/SavedKajianRecyclerAdapter.kt
@@ -0,0 +1,39 @@
+package com.purplepotato.kajianku.saved_kajian
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.purplepotato.kajianku.R
+import com.purplepotato.kajianku.core.domain.Kajian
+import com.purplepotato.kajianku.core.helper.DiffUtilItemCallback
+import com.purplepotato.kajianku.databinding.ItemSuggestedKajianBinding
+
+class SavedKajianRecyclerAdapter :
+    ListAdapter<Kajian, SavedKajianRecyclerAdapter.SavedKajianViewHolder>(
+        DiffUtilItemCallback.KAJIAN_DIFF_CALLBACK
+    ) {
+
+    inner class SavedKajianViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemSuggestedKajianBinding.bind(itemView)
+        fun bind(item: Kajian) {
+            with(binding) {
+                //assign value to item recycler view
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedKajianViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_suggested_kajian, parent, false)
+        return SavedKajianViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: SavedKajianRecyclerAdapter.SavedKajianViewHolder,
+        position: Int
+    ) {
+        holder.bind(getItem(position))
+    }
+}
