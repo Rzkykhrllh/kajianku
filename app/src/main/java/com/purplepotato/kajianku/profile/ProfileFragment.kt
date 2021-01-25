76db20706a883e8a62d7db2bae1b4bd3487ad27f
Rzkykhrllh <rizkykirigaya@gmail.com>
2021-01-25 08:25:01 +0800

    set username and email in Profile View

--- app/src/main/java/com/purplepotato/kajianku/profile/ProfileFragment.kt
+++ app/src/main/java/com/purplepotato/kajianku/profile/ProfileFragment.kt
@@ -1,12 +1,16 @@
 package com.purplepotato.kajianku.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.purplepotato.kajianku.MainActivity
 import com.purplepotato.kajianku.ViewModelFactory
+import com.purplepotato.kajianku.auth.AuthenticationActivity
 import com.purplepotato.kajianku.databinding.FragmentProfileBinding
 
 
@@ -32,6 +36,36 @@ class ProfileFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvLogOut.setOnClickListener {
+            viewModel.logout()
+        }
+
+        // Log out
+        viewModel.navigateToLogin.observe(viewLifecycleOwner, Observer {
+            val intent = Intent(activity, AuthenticationActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            activity?.finishAfterTransition()
+        })
+
+        // Set Username
+        viewModel.username.observe(viewLifecycleOwner, Observer {
+            if (viewModel.username != null){
+                binding.tvUsername.setText(viewModel.username.value)
+            }
+        })
+
+        // Set Username and email to view
+        viewModel.email.observe(viewLifecycleOwner, Observer {
+            if (viewModel.email != null){
+                binding.tvEmail.setText(viewModel.email.value)
+            }
+        })
     }
 
 }
--- app/src/main/java/com/purplepotato/kajianku/profile/ProfileViewModel.kt
+++ app/src/main/java/com/purplepotato/kajianku/profile/ProfileViewModel.kt
@@ -1,7 +1,51 @@
 package com.purplepotato.kajianku.profile
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.purplepotato.kajianku.core.data.KajianRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 class ProfileViewModel(private val repository: KajianRepository) : ViewModel() {
+
+    lateinit var auth: FirebaseAuth
+    lateinit var user : FirebaseUser
+
+
+
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private val _navigateToLogin = MutableLiveData<Boolean>()
+    val navigateToLogin : LiveData<Boolean>
+        get() = _navigateToLogin
+
+    private val _username = MutableLiveData<String?>()
+    val username : LiveData<String?>
+        get() = _username
+
+    private val _email = MutableLiveData<String?>()
+    val email : LiveData<String?>
+        get() = _email
+
+    init {
+        auth = FirebaseAuth.getInstance()
+        var user = auth.currentUser
+
+        user?.let {
+            _username.value = user.displayName
+            _email.value = user.email
+        }
+    }
+
+
+    fun logout(){
+
+        auth.signOut()
+        _navigateToLogin.value = true
+    }
 }
